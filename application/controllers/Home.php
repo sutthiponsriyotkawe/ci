6793cb2dcafff2e9ecbde580e21332221b5e15ab
samnith <tatsamnith083@gmail.com>
2016-11-26 16:18:50 +0700

    footer_view

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,14 @@
 class Home extends CI_Controller
 {
  public function  index(){
+     $this->load->view('layout/header_view');
      $this->load->view('home_views');
+
+ }
+ public function test(){
+     $this->load->view('layout/header_view');
+     $this->load->view('test_view');
+     $this->load->view('layout/footer_view');
  }
 }
 
---
+++ application/views/test_view.php
@@ -0,0 +1,10 @@
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium laudantium praesentium quas! Aperiam
+    consequatur debitis deleniti dolores facere fugit labore laborum provident, quam quasi quia, quod repellat, sed sunt
+    ut?
+</div>
+<div>Ab amet animi consequatur delectus deserunt minima optio quae. Alias aspernatur atque autem consequuntur dolorem
+    hic, inventore, ipsum magnam nemo non nulla officiis omnis placeat quasi, quod unde ut voluptas?
+</div>
+<div>Atque aut autem blanditiis, commodi, consectetur corporis dicta eius et eum fuga ipsam iure labore molestias
+    mollitia, officiis praesentium quae quis quos repudiandae rerum sequi suscipit tenetur vel veniam vitae?
+</div>
